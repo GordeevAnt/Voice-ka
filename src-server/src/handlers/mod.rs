f5fa717d6a360fdbd53a9f210e111293417a246f
GordeevAnt <antongordeev6721@gmail.com>
2026-05-02 14:30:13 +0600

    deleted unused imports

--- src-server/src/handlers/mod.rs
+++ src-server/src/handlers/mod.rs
@@ -3,36 +3,3 @@ pub mod auth;
 pub mod guild;
 pub mod message;
 pub mod room;
-
-// Экспортируем новые функции из guild
-pub use guild::{
-    handle_get_user_guilds, 
-    handle_create_guild, 
-    handle_join_guild, 
-    handle_get_guild_members,
-    handle_find_guild_by_id,
-    handle_get_online_guild_members,
-    handle_get_user_guilds_with_role,
-    CreateGuildData,
-};
-
-// Экспортируем новые функции из room
-pub use room::{
-    handle_get_guild_rooms,
-    handle_create_room,
-    handle_get_room_by_id,
-    CreateRoomData,
-};
-
-// Экспортируем новые функции из auth
-pub use auth::{
-    handle_login,
-    handle_register,
-    handle_logout,
-    validate_session,
-    handle_get_current_user,
-    handle_get_user_stats,
-    handle_update_user_profile,
-    LoginData,
-    RegisterData,
-};
--- src-server/src/main.rs
+++ src-server/src/main.rs
@@ -13,24 +13,9 @@ use ws::messages::{WsMessage, WsClientMessage};
 use uuid::Uuid;
 use chrono::Utc;
 
-use handlers::auth::{
-    handle_login, handle_register, handle_logout, validate_session, 
-    handle_get_current_user, handle_get_user_stats, handle_update_user_profile,
-    LoginData, RegisterData
-};
-use handlers::guild::{
-    handle_get_user_guilds, handle_create_guild, handle_join_guild, 
-    handle_get_guild_members, handle_find_guild_by_id, 
-    handle_get_online_guild_members, handle_get_user_guilds_with_role,
-    CreateGuildData
-};
-
 use handlers::room::{
     handle_get_guild_rooms, handle_create_room, handle_get_room_by_id,
     CreateRoomData
-};
-use handlers::message::{
-    handle_get_room_messages, handle_send_message
 };
 
 #[tokio::main]
