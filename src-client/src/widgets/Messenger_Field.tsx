2d02d340527bc9666001f1412f72546e36347a31
GordeevAnt <antongordeev6721@gmail.com>
2026-05-13 19:55:41 +0600

    fixed import

--- src-client/src/widgets/Messenger_Field.tsx
+++ src-client/src/widgets/Messenger_Field.tsx
@@ -1,5 +1,5 @@
 // widgets/Messenger_Field.tsx
-import { Messages_List } from "./Messages_List";
+import { Messages_List } from "../entities/Messages_List";
 import { Message_Input } from "../entities/Message_Input";
 import { wsService } from "../features/websocket.service";
 import { useEffect, useState } from "react";
